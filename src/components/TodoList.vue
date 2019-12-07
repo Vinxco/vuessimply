4c868b08474ff6673fcf4f1cd50f524efbc4bcd5
Vicente <vicente.codon@tecdata.es>
2019-12-07 20:57:30 +0100

    Linteo y primero pasos

---
+++ src/components/TodoList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <form id="app" @submit.prevent="createTask">
+      <label class="label" for="task">Nueva tarea: </label>
+      <input id="task" v-model="newTask" type="text" />
+
+      <input type="submit" value="crear tarea" />
+    </form>
+    <pre>
+       {{ $data }}
+    </pre>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    newTask: "",
+    tasks: []
+  }),
+  methods: {
+    createTask() {
+      let task = {
+        text: this.newTask,
+        completed: false
+      };
+      this.tasks.push(task);
+      this.newTask = "";
+      console.log(this.tasks);
+    }
+  }
+};
+</script>
+
+<style></style>
